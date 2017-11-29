129c157c93b3679e981d873f71dae6e0c752ef3c
NickDErrico <nickderrico@hotmail.com>
2017-11-29 16:55:43 -0700

    jquery excercises

--- app.js
+++ app.js
@@ -1,40 +1,43 @@
-console.log("Javascript is alive!");
-document.getElementById('greeting').innerHTML = "Hello, World!";
+$(document).ready(function() {
+  console.log("Javascript is alive!");
 
-let newImage = document.createElement("img")
-newImage.src = "http://49.media.tumblr.com/tumblr_m6qt1rjPSz1rxjzkho1_500.gif";
-document.getElementById(greeting.append(newImage))
+  $('#greeting').text("Hello, World!");
 
-itemImages = {
+  $('li').css('background-color', 'yellow');
+
+  let img = $('<img />').attr({
+    'src': `http://49.media.tumblr.com/tumblr_m6qt1rjPSz1rxjzkho1_500.gif`
+  }).appendTo('#greeting')
+  $('li')
+
+  itemImages = {
   0: "./images/milk.jpeg",
   1: "./images/honey.jpeg",
   2: "./images/water.jpeg",
   3: "./images/wine.jpeg",
   4: "./images/beer.jpeg"}
 
-let listItems = document.querySelectorAll('li')
-for (let i = 0; i < listItems.length; i++) {
-  listItems[i].style.backgroundColor = "yellow";
-  listItems[i].addEventListener('click', function() {
-    if (this.className !== 'selected') {
-      this.className = "selected"
-      document.querySelectorAll('img')[1].src = itemImages[i]
-    }
-    for (let j = 0; j < listItems.length; j++) {
-      if (j !== i) listItems[j].className = ""
-    }
-  })
-}
+  let listItems = document.querySelectorAll('li')
+  for (let i = 0; i < listItems.length; i++) {
+    listItems[i].addEventListener('click', function() {
+      if (this.className !== 'selected') {
+        this.className = "selected"
+        document.querySelectorAll('img')[1].src = itemImages[i]
+      }
+      for (let j = 0; j < listItems.length; j++) {
+        if (j !== i) listItems[j].className = ""
+      }
+    })
+  }
 
-let remove = document.getElementById('ghosting')
-remove.addEventListener('mouseover', function() {
-  remove.parentNode.removeChild(remove)
-})
-
-let twiceTheWidth = document.getElementById('resize')
-twiceTheWidth.addEventListener('mouseenter', function() {
-  twiceTheWidth.style.width = `${twiceTheWidth.scrollWidth * 2}px`
+  $('#ghosting').on('mouseover', function() {
+    $('#ghosting').remove('#ghosting')
   })
-  twiceTheWidth.addEventListener('mouseout', function() {
-    twiceTheWidth.style.width = `${twiceTheWidth.scrollWidth / 2}px`
+  $('#resize').on('mouseover', function() {
+    let oldWidth = $('#resize').width()
+    $('#resize').on('mouseleave', function() {
+      $("#resize").width(oldWidth)
+    })
+    $("#resize").width(oldWidth * 2)
+  })
 })
