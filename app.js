de14122ce0fc104e57aa976a5fdb607a39533569
NickDErrico <nickderrico@hotmail.com>
2017-11-30 13:05:24 -0700

    finished in jquery

--- app.js
+++ app.js
@@ -10,25 +10,24 @@ $(document).ready(function() {
   }).appendTo('#greeting')
   $('li')
 
-  itemImages = {
-  0: "./images/milk.jpeg",
-  1: "./images/honey.jpeg",
-  2: "./images/water.jpeg",
-  3: "./images/wine.jpeg",
-  4: "./images/beer.jpeg"}
+  $("li").on('click', function() {
+    $('li').removeClass('selected')
+    $(this).addClass('selected')
+    $('img:eq(1)').attr('src', "./images/" + $(this).text() + ".jpeg")
+  })
 
-  let listItems = document.querySelectorAll('li')
-  for (let i = 0; i < listItems.length; i++) {
-    listItems[i].addEventListener('click', function() {
-      if (this.className !== 'selected') {
-        this.className = "selected"
-        document.querySelectorAll('img')[1].src = itemImages[i]
-      }
-      for (let j = 0; j < listItems.length; j++) {
-        if (j !== i) listItems[j].className = ""
-      }
-    })
-  }
+  // let listItems = document.querySelectorAll('li')
+  // for (let i = 0; i < listItems.length; i++) {
+  //   listItems[i].addEventListener('click', function() {
+  //     if (this.className !== 'selected') {
+  //       this.className = "selected"
+  //       document.querySelectorAll('img')[1].src = itemImages[i]
+  //     }
+  //     for (let j = 0; j < listItems.length; j++) {
+  //       if (j !== i) listItems[j].className = ""
+  //     }
+  //   })
+  // }
 
   $('#ghosting').on('mouseover', function() {
     $('#ghosting').remove('#ghosting')
@@ -40,4 +39,50 @@ $(document).ready(function() {
     })
     $("#resize").width(oldWidth * 2)
   })
+
+  $("#reset").on('click', function() {
+    $("li").removeClass('selected')
+    $("img:eq(1)").attr('src', "./images/panic.jpeg")
+  })
 })
+
+$('html').keypress(function(event) {
+   if (event.keyCode >= 48 && event.keyCode <= 57) {
+     alert("I HATE NUMBERZZZ!");
+   };
+})
+
+neededkeys = [38,38,40,40,37,39,37,39,66,65], started = false, count = 0;
+$(document).keydown(function(e){
+	key = e.keyCode;
+	//Set start to true only if having pressed the first key in the konami sequence.
+	if(!started){
+		if(key == 38){
+			started = true;
+		}
+	}
+	//If we've started, pay attention to key presses, looking for right sequence.
+	if(started){
+		if(neededkeys[count] == key){
+			//We're good so far.
+			count++;
+		} else {
+			//Oops, not the right sequence, lets restart from the top.
+			reset();
+		}
+		if(count == 10){
+			//We made it! Put code here to do what you want when successfully execute konami sequence
+      alert("YOU ARE AN EVENT HANDLER GURUUUUUUUUU!")
+			//Reset the conditions so that someone can do it all again.
+			reset();
+		}
+	} else {
+	//Oops.
+		reset();
+	}
+});
+//Function used to reset us back to starting point.
+function reset() {
+	started = false; count = 0;
+	return;
+}
